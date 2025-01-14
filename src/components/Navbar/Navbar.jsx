c366eaefab8a3d3a12f8b140beddfe3e473e26e7
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-01-14 23:21:27 +0600

    Navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_icon from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
@@ -16,7 +21,17 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-right">
+        <img src={search_icon} alt="" className='icons' />
+        <p>Children </p>
+        <img src={bell_icon} alt="" className='icons' />
+        <div className="navbar-profile">
+          <img src={profile_icon} alt="" className='profile' />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p>SING Out of Netflix</p>
+          </div>
 
+        </div>
       </div>
     </div>
   )
